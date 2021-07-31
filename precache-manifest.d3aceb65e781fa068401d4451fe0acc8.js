9f6eb87546df743615778879135ce94625e72ab7
qg <274293376@qq.com>
2021-07-31 16:36:13 +0800

    deploy

---
+++ precache-manifest.d3aceb65e781fa068401d4451fe0acc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13004190194229ae34ec",
+    "url": "/cashbook-website/css/app.3705db41.css"
+  },
+  {
+    "revision": "571c27721bf11313cfb15b59ab633210",
+    "url": "/cashbook-website/index.html"
+  },
+  {
+    "revision": "13004190194229ae34ec",
+    "url": "/cashbook-website/js/app.62414085.js"
+  },
+  {
+    "revision": "36dae2e83e55aa7d5da7",
+    "url": "/cashbook-website/js/chunk-vendors.b9ac2cc3.js"
+  },
+  {
+    "revision": "b384eafa891f01b91d307c7a728467fc",
+    "url": "/cashbook-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/cashbook-website/robots.txt"
+  }
+]);
